269dc601b2b2f2b0fdb63165d7d586abbaff89de
haoruiwang <1905811497@qq.com>
2024-01-06 11:52:33 +0800

    [what][build][codec] rockchip 解码主流程 (part 1)
    [how]
    1. 工程结构初步搭建
    2. 编译适配
    3. 初始化相关主流程

---
+++ Codec/Rockchip/RKDecoder.h
@@ -0,0 +1,63 @@
+//
+// RKDecoder.h
+//
+// Library: Common
+// Package: Codec
+// Module:  Rockchip
+// 
+// Note : This file is modified from mpp/test/mpi_dec_test.c
+//        Date : 2024-01-06
+//        Hash : 5f1cd0f12d643f017aea61a196b3b766df511bf6
+// Copyright 2015 Rockchip Electronics Co. LTD
+//
+
+#pragma once
+
+#include <mutex>
+
+#include "RKCommon.h"
+
+namespace Mmp
+{
+namespace Codec
+{
+
+class RKDecoder : public AbstractDecoder
+{
+public:
+    using ptr = std::shared_ptr<RKDecoder>();
+public:
+    RKDecoder();
+    virtual ~RKDecoder() = default;
+public:
+    void SetParameter(Any parameter) override;
+    Any GetParamter() override;
+    bool Init() override;
+    void Uninit() override;
+    bool Start() override;
+    void Stop() override;
+    bool Push(AbstractPack::ptr pack) override;
+    bool Pop(AbstractFrame::ptr& frame) override;
+    bool CanPush() override;
+    bool CanPop() override;
+    const std::string& Description() override;
+public:
+    virtual CodecType GetCodecType() = 0;
+private:
+    bool CreateMPI();
+    void DestroyMPI();
+    bool ReinitDecConfig();
+    bool InitRkPacket();
+    void UnitRkPacket();
+private:
+    std::mutex      _mtx;
+    MppApi*         _mpi;
+    MppCtx          _ctx;
+    uint64_t        _timeout;
+    MppBufferGroup  _frameGroup;
+    uint32_t        _maxFrame;
+    MppPacket       _rkPacket;
+};
+
+} // namespace Codec
+} // namespace Mmp
